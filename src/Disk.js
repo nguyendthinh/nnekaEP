786b431ac6de2eb5902e35a1f68b9abf2d16fef4
Thinh Nguyen <nguyendthinh@outlook.com>
2017-11-30 20:43:29 -0500

    played around with some audio options

--- src/Disk.js
+++ src/Disk.js
@@ -2,11 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Disk extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      play: false
+    }
+  }
+
+  music(){
+    console.log("working")
+    if (this.state.play === false) {
+      this.setState({
+        play: true
+      })
+    } else {
+      this.setState({
+        play: false
+      })
+    }
+  }
+
   render() {
+
     return (
       <div style={this.props.divStyle} className={`Disk ${this.props.currentClass}`}>
-        Disk
-        <div></div>
+        <div>
+          {this.state.play === false ? (
+            <div onClick={this.music.bind(this)} className="glyphicon glyphicon-play"></div>
+          ) : (
+            <div onClick={this.music.bind(this)} className="glyphicon glyphicon-pause"></div>
+
+          )}
+
+        </div>
       </div>
     );
   }
