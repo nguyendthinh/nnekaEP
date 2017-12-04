6e54abf31dd9930d2b6d408af9e1ec27973cd88c
Thinh Nguyen <nguyendthinh@outlook.com>
2017-12-04 13:34:24 -0500

    play, pause, continue track feature

--- src/Disk.js
+++ src/Disk.js
@@ -1,29 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import mp3_file from './audio/SampleOne.mp3'
+import mp3_file2 from './audio/SampleTwo.mp3'
+
 class Disk extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      play: false
+      play: false,
+      trackTracker: 0,
+      currentSong: mp3_file
     }
+    this.trackList = [mp3_file, mp3_file2]
   }
 
   music(){
-    console.log("working")
+    var audio = document.getElementById('audio')
     if (this.state.play === false) {
       this.setState({
         play: true
       })
+      audio.play()
     } else {
       this.setState({
         play: false
       })
+      audio.pause()
     }
   }
 
+  changeTrack(){
+    console.log('works')
+    var audio = document.getElementById('audio');
+
+      if (this.state.trackTracker === 0) {
+        var newSong = this.state.trackTracker + 1;
+        this.setState({
+          trackTracker: newSong,
+          currentSong: this.trackList[newSong]
+        })
+        setTimeout( function() {
+                        audio.play();
+                    }, 1);
+      } else if (this.state.trackTracker === 1) {
+        this.setState({
+          currentSong: mp3_file,
+          trackTracker: 0
+        })
+        setTimeout( function() {
+                        audio.play();
+                    }, 1);
+      }
+  }
+
   render() {
+
+    var audio = document.getElementById('audio')
 
     return (
       <div style={this.props.divStyle} className={`Disk ${this.props.currentClass}`}>
@@ -36,6 +70,7 @@ class Disk extends Component {
           )}
 
         </div>
+        <audio onEnded={this.changeTrack.bind(this)} id="audio" src={this.state.currentSong} controls></audio>
       </div>
     );
   }
