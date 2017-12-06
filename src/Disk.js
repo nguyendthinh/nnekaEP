e4845b6b5638251358e836474208b1fbbab5eefd
Thinh Nguyen <nguyendthinh@outlook.com>
2017-12-06 16:32:57 -0500

    fixed nav bar positioning

--- src/Disk.js
+++ src/Disk.js
@@ -10,17 +10,17 @@ class Disk extends Component {
     return (
       <div style={this.props.divStyle} className={`Disk ${this.props.currentClass}`}>
         <div id="spinDiv"></div>
-        <div onClick={this.props.backTrack} className="glyphicon glyphicon-backward"></div>
-        <div>
-          {this.props.play === false ? (
-            <div onClick={this.props.music} className="glyphicon glyphicon-play"></div>
-          ) : (
-            <div onClick={this.props.music} className="glyphicon glyphicon-pause"></div>
+          <div onClick={this.props.backTrack} className="glyphicon glyphicon-backward"></div>
+          <div>
+            {this.props.play === false ? (
+              <div onClick={this.props.music} className="glyphicon glyphicon-play"></div>
+            ) : (
+              <div onClick={this.props.music} className="glyphicon glyphicon-pause"></div>
 
-          )}
+            )}
 
-        </div>
-        <div onClick={this.props.nextTrack} className="glyphicon glyphicon-forward"></div>
+          </div>
+          <div onClick={this.props.nextTrack} className="glyphicon glyphicon-forward"></div>
 
         <audio onEnded={this.props.changeTrack} id="audio" src={this.props.currentSong}></audio>
       </div>
